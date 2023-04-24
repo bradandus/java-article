1d91f8ad6008d84dc2baf4bf769f2f659fd0126f
bradandus <xienjava@gmail.com>
2023-04-24 13:24:09 +0900

    아티클 컨트롤러 생성 완료

--- src/article/App.java
+++ src/article/App.java
@@ -58,50 +58,16 @@ public class App{
 				articleController.doWrite();
 
 			} else if (command.startsWith("article detail")) {
-				String[] commandBits = command.split(" ");
-				int id = Integer.parseInt(commandBits[2]);
-				Article foundArticle = getArticleById(id);
-				if (foundArticle == null){
-					System.out.printf("%d 번째 게시물을 찾을 수 없습니다.%n", id);
-				}else {
-					foundArticle.increasHit();
-					System.out.printf("번호 : %d%n", foundArticle.id);
-					System.out.printf("제목 : %s%n", foundArticle.title);
-					System.out.printf("내용 : %s%n", foundArticle.body);
-					System.out.printf("등록 시간 : %s%n", foundArticle.regDate);
-					System.out.printf("조회수 : %d%n", foundArticle.hit);
-				}
+				
+				articleController.doDetail(command);
+
 
 			} else if (command.startsWith("article delete")) {
-				String[] commandBits = command.split(" ");
-				int id = Integer.parseInt(commandBits[2]);
-				int foundIndex = getArticleIndexById(id);
-				if (foundIndex == -1) {
-					System.out.printf("%d 번 게시물을 찾을 수 없습니다.%n", id);
-				}else {
-					articles.remove(foundIndex);
-					System.out.printf("%d 번 게시물을 삭제했습니다.%n", id);
-				}
-				
-
+				articleController.doDelete(command);
 				
 				
 			} else if (command.startsWith("article modify")) {
-				String[] commandBits = command.split(" ");
-				int id = Integer.parseInt(commandBits[2]);
-				Article foundArticle = getArticleById(id);
-				if (foundArticle == null){
-					System.out.printf("%d 번째 게시물을 찾을 수 없습니다.", id);
-				}else {
-					System.out.printf("%d 번 게시물을 찾았습니다.%n", id);
-					System.out.printf("수정할 제목 : ");
-					foundArticle.title = sc.nextLine();
-					System.out.printf("수정할 내용 : ");
-					foundArticle.body = sc.nextLine();
-					foundArticle.regDate = Util.getRegDate();
-					System.out.printf("%d 게시물이 수정 완료 되었습니다.%n", id);
-					
-				}
+				articleController.doModify(command);
 				
 			} else if(command.equals("member join")) {
 				memberController.doJoin();
@@ -123,28 +89,6 @@ public class App{
 		;
 	}
 	
-	// 게시물 아이디로 찾기
-	private Article getArticleById(int id) {
-		int index = getArticleIndexById(id);
-		if (index != -1) { // 일치하는 index가 있는 경우 (-1은 찾지 못한 경우)
-			return articles.get(index);
-		}
-		return null;
-	}
-	
-	// 게시물 아이디로 인덱스 찾기
-	private int getArticleIndexById(int id) {
-		int i = 0;
-		for (Article article : articles) {
-			if (article.id == id) {
-				return i;
-			}
-			i++;
-		}
-		
-		return -1;
-	}
-
 
 	private void makeTestData() {
 		// TODO Auto-generated method stub
